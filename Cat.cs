78b8b3ba38e6d5442665bdadd9b1cdb2427511f1
Olexandr <124339852+OLeksandr344@users.noreply.github.com>
2023-11-01 23:27:11 +0200

    1

---
+++ Cat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatTask
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public  class Cat
+    {
+        
+
+        public string Name { get; }
+        public Gender Gender { get; }
+        private double _energy;
+        public static readonly double MaxEnergy = 20;
+        public static readonly double MinEnergy = 0;
+        public static readonly double SleepEnergyGain = 10;
+        public static readonly double JumpEnergyDrain = 0.5;
+      
+        public double Energy
+        {
+            get { return _energy; }
+            set
+            {
+                if (value < MinEnergy)
+                {
+                    throw new Exception("Not enough energy to jump");
+                }
+                else if (value > MaxEnergy)
+                {
+                    _energy = MaxEnergy;
+                }
+                else
+                {
+                    _energy = value;
+                }
+            }
+        }
+           
+        public Cat (string name, Gender gender)
+        {
+
+            Energy = MaxEnergy;
+            
+            Name = name;
+            Gender = gender;
+        }
+        public void Jump()
+        {
+            Random random = new Random();
+            double success = 0.8;
+            if (random.NextDouble() <= success)
+            {             
+                Energy -= JumpEnergyDrain;             
+            }
+            else
+            {
+                Energy -= JumpEnergyDrain*2;
+                Console.WriteLine("Киця впала!");
+            }
+            
+        }
+        public void Sleep()
+        {
+            Energy += SleepEnergyGain;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using CatTask;
+using System.Net.Http.Headers;
+
+Cat Garfield = new Cat("Garfield", Gender.Male);
+Cat Garfield1 = new Cat("Garfield1", Gender.Male);
+
+Console.WriteLine(Garfield.Energy);
+Console.WriteLine(Garfield1.Energy);
+Console.WriteLine("------------------");
+Garfield.Jump();
+Console.WriteLine(Garfield.Energy);
+Console.WriteLine(Garfield1.Energy);
+Garfield.Sleep();
+Garfield.Jump();
+Garfield.Jump();
+Garfield.Jump();
+Garfield.Jump();
+Garfield.Jump();
+Garfield.Jump();
+Garfield.Jump();
+Console.WriteLine(Garfield.Energy);
+
+
+
+
